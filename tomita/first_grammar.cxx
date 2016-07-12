baf30784430932bdd56d6814f42718c0ffcd7ca6
RooshRoosh <roosh90@mail.ru>
2016-07-12 23:08:52 +0500

    более широкие правила для обработки запросов

--- tomita/first_grammar.cxx
+++ tomita/first_grammar.cxx
@@ -1,3 +1,9 @@
 #encoding "utf8"
 
-SimpleQuestionStart -> 'как' (Adv interp(Q.Adv)) Word<gram="inf">interp(Q.Verb);
+ShortTermRus -> Word<wfl="[А-Яа-я-]{1,9}">(Word<wfl="[0-9]{1,4}">);
+ShortTermEng -> Word<wfl="[A-Za-z0-9-]*">(Word<wfl="[0-9]{1,4}">);
+ShortTerm-> ShortTermEng|ShortTermRus;
+//PersonalPronoun -> "себе" | "себя" | "тебя" | "тебе";
+PrepT -> "c"|Prep;
+NounGroup -> (PrepT) (Adj) ShortTerm|Noun;
+SimpleQuestionStart -> 'как' (Word<gram="~V">+ interp(Q.FirstWords::not_norm)) Word<gram="V"> interp(Q.Verb::not_norm) (NounGroup interp(Q.NounAfterVerb::not_norm));
